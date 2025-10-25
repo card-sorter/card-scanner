24c98beb4f500617356978b3da9f111ea1486ec7
Arczodiak <gilbraith.liucaesar@gmail.com>
2025-10-25 14:39:17 +0800

    attempt wider configuration system

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 MODEL_PATH = "model/best.pt"
 PORT = "8000"
-HOSTNAME = "0.0.0.0"
+HOSTNAME = "localhost"
 UPDATE_HOUR = 2
 UPDATE_MINUTE = 30
-DATABASE = {}
+DATABASE = "./image_hashes.db" 
 GAME_CATEGORY = 3
 HASH_REPOSITORY = "https://github.com/card-sorter/hashes.git"
 HASH_PATH = "./hashes"
